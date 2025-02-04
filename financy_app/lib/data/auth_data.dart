7f6aca62e8ac6abf937d9f2d82366bee4c2b8ff9
Emerson Viçosa <emersonvicosa@gmail.com>
2025-02-03 22:14:48 -0300

    terminando primeiro video

---
+++ financy_app/lib/data/auth_data.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthenticationDataSource {
+  Future<void> register(String email, String password, String PasswordConfirm);
+  Future<void> login(String email, String password);
+}
+
+class AuthenticationRemote extends AuthenticationDataSource {
+  @override
+  Future<void> login(String email, String password) async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: email.trim(), password: password.trim());
+  }
+
+  @override
+  Future<void> register(String email, String password, String PasswordConfirm) async {
+    if(PasswordConfirm == password) {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email.trim(), password: password.trim());
+    }
+  }
+}
